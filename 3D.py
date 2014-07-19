15a1ec755f7502e45a645bc6ea6bf96ba9c63263
liran319 <liran319@gmail.com>
2014-07-19 13:16:38 +0800

    Update

--- 3D.py
+++ 3D.py
@@ -1,31 +1,16 @@
-import matplotlib.path as mpath
-import matplotlib.patches as mpatches
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import numpy as np
 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-fig, ax = plt.subplots()
+u = np.linspace(0, 2 * np.pi, 200)
+v = np.linspace(0, np.pi, 100)
 
-Path = mpath.Path
-path_data = [
-    (Path.MOVETO, (1.58, -2.57)),
-    (Path.CURVE4, (0.35, -1.1)),
-    (Path.CURVE4, (-1.75, 2.0)),
-    (Path.CURVE4, (0.375, 2.0)),
-    (Path.LINETO, (0.85, 1.15)),
-    (Path.CURVE4, (2.2, 3.2)),
-    (Path.CURVE4, (3, 0.05)),
-    (Path.CURVE4, (2.0, -0.5)),
-    (Path.CLOSEPOLY, (1.58, -2.57)),
-    ]
-codes, verts = zip(*path_data)
-path = mpath.Path(verts, codes)
-patch = mpatches.PathPatch(path, facecolor='r', alpha=0.5)
-ax.add_patch(patch)
+x = 10 * np.outer(np.cos(u), np.sin(v))
+y = 10 * np.outer(np.sin(u), np.sin(v))
+z = 10 * np.outer(np.ones(np.size(u)), np.cos(v))
+ax.plot_surface(x, y, z, rstride=4, cstride=4, color='b')
 
-# plot control points and connecting lines
-x, y = zip(*path.vertices)
-line, = ax.plot(x, y, 'go-')
-
-ax.grid()
-ax.axis('equal')
 plt.show()
